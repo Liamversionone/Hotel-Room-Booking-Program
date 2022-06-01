9082cda9b78e668592513e357c506c46a32f8229
EchoAgent <91441032+Liamversionone@users.noreply.github.com>
2022-05-31 20:09:56 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import mod.Overseer;
 
 public class Main {
 
+	//This method is used to run and execute the program.
 	public static void main(String[] args0) {
 		Overseer over = new Overseer();
 	}
